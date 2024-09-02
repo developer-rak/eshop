105910b03045e47a8bdeb7dc5c943472d0915e26
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-09-02 19:09:24 +0500

    loader and adding firebase

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./auth.scss";
 
 import { Link } from 'react-router-dom';
@@ -6,17 +6,51 @@ import Card from '../../components/card/Card';
 import registerImg from "../../assets/register.png";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+
+  const registerUser = (e) => {
+    e.preventDefault()
+      console.log(email, password, cPassword)
+  }
+
   return (
     <section className="container auth">
       <Card>
         <div className="form">
           <h2>Register</h2>
-          <form>
-            <input type="email" placeholder='Email' required />
-            <input type="password" placeholder='Password' required />
-            <input type="password" placeholder='Confirm Password' required />
-            <button className="--btn --btn-primary --btn-block">Register</button>
+
+          <form onSubmit={registerUser}>
+            <input 
+              type="email" 
+              placeholder='Email' 
+              required 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)} 
+            />
+            <input 
+              type="password" 
+              placeholder='Password' 
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input 
+              type="password" 
+              placeholder='Confirm Password' 
+              required 
+              value={cPassword}
+              onChange={(e) => setCPassword(e.target.value)}
+            />
+            <button 
+              className="--btn --btn-primary --btn-block" 
+              type='submit'
+            >
+              Register
+            </button>
           </form>
+
           <span className='register'>
             <p>Already an account? </p>
             <Link to="/login">Login</Link>
