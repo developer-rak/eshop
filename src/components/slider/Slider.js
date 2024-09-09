31eeaead16ff68a4769070a6ba1a553ad9941708
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-09-09 10:50:10 +0500

    added slider arrow functionality

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -4,18 +4,31 @@ import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
 import { sliderData } from './slider-data';
 
 const Slider = () => {
-   const [currentSlide, setCurrentSlide] = useState(0)
+   const [currentSlide, setCurrentSlide] = useState(0);
+   const sliderLength = sliderData.length;
+   console.log(sliderLength)
+
+   const nextSlide = () => {
+      setCurrentSlide(currentSlide === sliderLength - 1 ? 0 : currentSlide + 1)
+   }
+
+   const preSlide = () => {
+      setCurrentSlide(currentSlide === 0 ? sliderLength - 1 : currentSlide - 1)
+   }
 
   return (
     <div className='slider'>
-      <AiOutlineArrowLeft className='arrow prev' />
-      <AiOutlineArrowRight className='arrow next' />
+      <AiOutlineArrowLeft className='arrow prev' onClick={preSlide} />
+      <AiOutlineArrowRight className='arrow next' onClick={nextSlide} />
 
       {sliderData.map((slide, index) => {
-         const {image, heading, desc} = slide
+         const {image, heading, desc} = slide;
 
          return (
-            <div key={index} className={index === currentSlide ? "slide current" : "slide"}>
+            <div 
+               key={index} 
+               className={index === currentSlide ? "slide current" : "slide"}
+            >
                {index === currentSlide && (
                   <>
                      <img src={image} alt='slideImg' />
@@ -23,7 +36,7 @@ const Slider = () => {
                         <h2>{heading}</h2>
                         <p>{desc}</p>
                         <hr />
-                        <a href="#product" className='--btn --btn-primary'>
+                        <a href="##" className='--btn --btn-primary'>
                            Shop Now
                         </a>
                      </div>
